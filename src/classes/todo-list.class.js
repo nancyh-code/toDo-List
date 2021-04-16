74f0a9fad9256452eb38ddab0f03e04da9f0612f
nancy hernandez <hernandezyane@hotmail.com>
2021-04-16 10:11:30 -0400

    first commit

---
+++ src/classes/todo-list.class.js
@@ -0,0 +1,23 @@
+
+export class TodoList {
+  
+  constructor() {
+    this.todos = [];
+  }
+
+  nuevoTodo(todo) {
+    this.todos.push(todo);
+  }
+
+  eliminarTodo(id) {
+
+  }
+
+  marcarCompletado(id) {
+
+  }
+
+  eliminarCompletados() {
+    
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { Todo, TodoList } from './classes';
+import './styles.css';
+
+const todoList = new TodoList();
+
+const tarea = new Todo('Aprender JavaScript');
+todoList.nuevoTodo(tarea);
+
+console.log(todoList);
